64509ae815d0bf23aae70dd6a39e9fe35fdf81de
Erik Hübl <this.crimson@gmail.com>
2022-12-11 14:03:44 +0100

    made event handler

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
-const { REST, Routes,  Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const { REST, Routes,  Client, GatewayIntentBits, EmbedBuilder, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const TOKEN = process.env.BOT_TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
     client.user.setActivity('in my support server', {type: "PLAYING"});
@@ -11,14 +22,27 @@ client.on('ready', () => {
 });
 
 client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
 
-  if (interaction.commandName === 'ping') {
+    if(!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.log(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    }
+  
+    /* if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
-  }
-  else if (interaction.commandName === 'info') {
+    }
+    else if (interaction.commandName === 'info') {
     await interaction.reply({ embeds: [informationEmbed] });
-  }
+    } */
 });
 
 const informationEmbed = new EmbedBuilder()
@@ -36,7 +60,7 @@ const informationEmbed = new EmbedBuilder()
     .setTimestamp()
     .setFooter({text: 'This bot is still under construction. Please be kind with it!'});
 
-const commands = [
+/* const commands = [
   {
     name: 'ping',
     description: 'Replies with Pong!',
@@ -45,16 +69,13 @@ const commands = [
     name: 'info',
     description: 'Sends an information about the bot',
   },
-];
+]; */
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
-
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
