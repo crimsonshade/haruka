451b6c8a6461ba460a0344a9f7b16f0bbe926259
Erik Hübl <this.crimson@gmail.com>
2022-12-11 16:28:17 +0100

    make command handler and deployment

--- bot.js
+++ bot.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
 const TOKEN = process.env.BOT_TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
 
 // Whe nthe client is ready, run this code
 client.once(Events.ClientReady, c => {
@@ -12,5 +18,40 @@ client.once(Events.ClientReady, c => {
     console.log('---------------------------------');
 });
 
+get_commands();
+
+// Receiving command interactions
+client.on(Events.InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if(!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch(error) {
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
 // Log in to Discord with your client's token
 client.login(TOKEN);
+
+async function get_commands() {
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for(const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
--- commands/user.js
+++ commands/user.js
@@ -4,7 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('user')
         .setDescription('Provies information about the user'),
-    async axecute(interaction) {
+    async execute(interaction) {
         await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`)
     },
 };
