966747f7d5d6e6350b7b6b467b8969dd530009b9
Erik Hübl <this.crimson@gmail.com>
2022-12-11 21:46:17 +0100

    fixed some typos

--- commands/info.js
+++ commands/info.js
@@ -14,14 +14,14 @@ module.exports = {
         .setFooter({ text: 'Still under construction. `written by Crimsonshade`' })
         .setDescription(`Here are some informations about me! Feel free to use my commands Senpai!`)
         .addFields(
-            { name: 'Created:', value: 'I was created on `' + client.user.createdAt + '`', inline: true},
-            { name: `Creator:`, value: 'My creater is **Crimsonshade#2000** `(my one and only Darling 😽)`', inline: true}, 
+            { name: 'Created:', value: 'I was created on `' + client.user.createdAt + '`'},
+            { name: `Creator:`, value: 'My creater is **Crimsonshade#2000** `(my Darling 😽)`'}, 
             { name: 'Commands:', value: 'Here are some commands, you can give me master!'},
             { name: '`ping`', value: 'I just reply with "pong!"', inline: true},
             { name: '`user`', value: 'I show you some informations about yourself!', inline: true},
             { name: '`server`', value: 'I give you some informations about your server!', inline: true },
             { name: '`info`', value: "This Embed right here 😺", inline: true},
-            { name: '`anime-image`', value: 'I show you a beatiful image/gif, based on the category you like!', inline: true}
+            { name: '`anime`', value: 'the `sfw` subcommand shows you some funny photos!\nThe `nsfw` does something else 😼', inline: true}
         )
         await interaction.reply({ embeds: [infoEmbed] });
     },
