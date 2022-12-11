ad9bd563c26bd4659595ea1198dc9fdfab7f4528
Erik Hübl <this.crimson@gmail.com>
2022-12-11 17:34:39 +0100

    update: ready event

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = { 
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        client.user.setActivity('at your command', {type: ActivityType.Playing });
         console.log(`----------------------------------------------`);
         console.log(`SENPAI! ${client.user.tag} just woke up!`);
         console.log(`----------------------------------------------`);
